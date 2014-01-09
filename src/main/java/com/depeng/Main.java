f0aaa60589c918ee84d4bd3e6e8ebfc506790ac0
Cong Depeng <congdepeng@gmail.com>
2014-01-09 23:08:11 +0800

    ddd

---
+++ src/main/java/com/depeng/Main.java
@@ -0,0 +1,15 @@
+package com.depeng;
+
+/**
+ * Created by depeng on 9/1/14.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("xxx");
+    }
+
+    public static void method() {
+        System.out.println("xxxx");
+    }
+
+}
---
+++ src/test/java/com/depeng/MainTest.java
@@ -0,0 +1,17 @@
+package com.depeng;
+
+import org.junit.Test;
+
+/**
+ * Created by depeng on 9/1/14.
+ */
+public class MainTest {
+
+    @Test
+    public void testMethod() {
+        System.out.println("abc");
+        Main.method();
+    }
+
+
+}
